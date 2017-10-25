c61a8b92360d0c22e61eca39063e150abf39282b
EduardoCruzProductions <eduardo_acj2@hotmail.com>
2017-10-25 15:19:10 -0200

    readequacao de valores

--- app/src/main/java/eduardocruzproductions/myclothesstock/Main.java
+++ app/src/main/java/eduardocruzproductions/myclothesstock/Main.java
@@ -68,20 +68,26 @@ public class Main extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_venda) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pagamento) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_addCliente) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_addProduto) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_trocaProduto) {
+
+        } else if (id == R.id.nav_relatorio_cliente) {
+
+        } else if (id == R.id.nav_relatorio_produto) {
+
+        } else if (id == R.id.nav_relatorio_venda) {
+
+        } else if (id == R.id.nav_relatorio_pagamento) {
 
         }
 
