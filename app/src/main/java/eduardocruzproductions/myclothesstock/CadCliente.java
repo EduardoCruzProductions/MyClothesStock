38628c7fd308eabee6400e350d803ffde98e72fb
EduardoCruzProductions <eduardo_acj2@hotmail.com>
2017-12-12 17:14:15 -0200

    implementacao do metodo adicionar

--- app/src/main/java/eduardocruzproductions/myclothesstock/CadCliente.java
+++ app/src/main/java/eduardocruzproductions/myclothesstock/CadCliente.java
@@ -6,8 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import eduardocruzproductions.myclothesstock.entidades.Cliente;
+import eduardocruzproductions.myclothesstock.util.ValidadorCPF;
 
 public class CadCliente extends AppCompatActivity {
+
+    EditText nome;
+    EditText cpf;
+    EditText data_nascimento;
+    EditText telefone;
+    EditText email;
+
+    private Cliente cliente = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +34,82 @@ public class CadCliente extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        nome = (EditText) findViewById(R.id.cadCliente_editText_nome);
+        cpf = (EditText) findViewById(R.id.cadCliente_editText_cpf);
+        data_nascimento = (EditText) findViewById(R.id.cadCliente_editText_dataNascimento);
+        telefone = (EditText) findViewById(R.id.cadCliente_editText_telefone);
+        email = (EditText) findViewById(R.id.cadCliente_editText_email);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void adicionarCliente(View view){
+
+        String nome = this.nome.getText().toString();
+        String cpf = this.cpf.getText().toString();
+        String data_nascimento = this.data_nascimento.getText().toString();
+        String telefone = this.telefone.getText().toString();
+        String email = this.email.getText().toString();
+
+        if(nome.isEmpty() || cpf.isEmpty() || telefone.isEmpty()){
+
+            Toast.makeText(getApplicationContext(), R.string.cadCliente_error_preencherRequisitados, Toast.LENGTH_LONG).show();
+
+        }else{
+
+            if(ValidadorCPF.isCPF(cpf)){
+
+                boolean proceed = true;
+                Date data = new Date();
+
+                if(!data_nascimento.isEmpty()){
+
+                    try{
+
+                        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+                        data =  formato.parse(data_nascimento);
+
+                    }catch(Exception e){
+
+                        proceed = false;
+                        Toast.makeText(getApplicationContext(), R.string.cadCliente_error_invalidDateFormat, Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+                if(proceed){
+
+                    try{
+
+                        cliente = new Cliente();
+                        cliente.setNome(nome);
+                        cliente.setCpf(cpf);
+                        cliente.setData_nascimento(data);
+                        cliente.setTelefone(telefone);
+                        cliente.setEmail(email);
+
+                        cliente.save();
+
+                        finish();
+
+                    }catch (Exception e){
+
+                        Toast.makeText(getApplicationContext(), R.string.error_falha_registro, Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+
+
+            }else{
+
+                Toast.makeText(getApplicationContext(), R.string.cadCliente_error_invalidCPF, Toast.LENGTH_LONG).show();
+
+            }
+
+        }
+
     }
 
 }
---
+++ app/src/main/java/eduardocruzproductions/myclothesstock/entidades/Cliente.java
@@ -0,0 +1,58 @@
+package eduardocruzproductions.myclothesstock.entidades;
+
+
+import com.orm.SugarRecord;
+
+import java.util.Date;
+
+public class Cliente extends SugarRecord{
+
+    private String nome;
+    private String cpf;
+    private Date data_nascimento;
+    private String telefone;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(Date data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
